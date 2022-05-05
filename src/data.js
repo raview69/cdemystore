58825f4d6a678a6d56880c243556c4c4b435eb21
raview69 <raviwicaksono@aol.com>
2022-05-05 11:27:32 +0700

    add product

--- src/data.js
+++ src/data.js
@@ -5,9 +5,24 @@ export const inventoryData = [
 		img: "https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg",
 	},
 	{
+		name: "Java Hat",
+		price: 19.99,
+		img: "https://cdn.shopify.com/s/files/1/0090/2932/0804/products/mockup-329d8e68_720x.jpg",
+	},
+	{
+		name: "JavaScript Hat",
+		price: 20.99,
+		img: "https://cdn.shopify.com/s/files/1/0090/2932/0804/products/mockup-f057d044_720x.jpg",
+	},
+	{
 		name: "Learn From Home Joggers",
 		price: 45.99,
 		img: "https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sweatpants.jpg",
+	},
+	{
+		name: "Learn From Home Joggers, Navy",
+		price: 45.99,
+		img: "https://cdn.shopify.com/s/files/1/0090/2932/0804/products/mockup-e855ce77_720x.jpg",
 	},
 	{
 		name: "Java Tee",
@@ -28,6 +43,11 @@ export const inventoryData = [
 		name: "Hello World Hoodie",
 		price: 49.99,
 		img: "https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hoodie.jpg",
+	},
+	{
+		name: "Hello World Hoodie, Navy",
+		price: 49.99,
+		img: "https://cdn.shopify.com/s/files/1/0090/2932/0804/products/mockup-1ff7ba13_720x.jpg",
 	},
 ];
 
